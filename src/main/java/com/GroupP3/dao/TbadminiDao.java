174ddf2839d61195741ea42f2f0d062ae4b153e7
Romeo <romeomelgar23@gmail.com>
2020-04-30 00:45:16 -0600

    se trabajo en el paquete dao admin

--- src/main/java/com/GroupP3/dao/TbadminiDao.java
+++ src/main/java/com/GroupP3/dao/TbadminiDao.java
@@ -1,5 +1,36 @@
 package com.GroupP3.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.GroupP3.model.Tbadmini;
+
 public class TbadminiDao {
 
+	public List<Tbadmini> confirmacionTbadmini(Tbadmini a){
+		List<Tbadmini> result = new ArrayList();
+		
+		EntityManager em;
+		EntityManagerFactory emf;
+		
+		emf = Persistence.createEntityManagerFactory("laboratorio-2");
+		em = emf.createEntityManager();
+			
+		try {
+			em.getTransaction().begin();
+			result = em.createQuery("from Tbadmini as a where a.cmpPass='"+a.getCmpUsser()+"' and a.cmpUsser = '"+a.getCmpPass()+"'").getResultList();
+			em.getTransaction().commit();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Error" + e.getMessage());
+		}  
+		 
+		
+		return result;
+	}
 }
